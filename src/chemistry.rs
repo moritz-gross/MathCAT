d5fbcaff26e279d1e06d8cfbda14b3d484b85991
mgros <hi@mgross.dev>
2026-09-26 19:08:49 +0200

    undo changes in `is_structural`

--- src/chemistry.rs
+++ src/chemistry.rs
@@ -1298,7 +1298,8 @@ fn is_structural(elements: &[NameStr<'_>]) -> bool {
     assert!(elements.len() > 1);   // already handled
 
     // debug!("is_structural: {:?}", elements);
-    let element_set: HashSet<_> = elements.iter().map(|e| as_str!(*e)).collect();
+    let mut element_set = HashSet::with_capacity(elements.len());
+    elements.iter().for_each(|e| {element_set.insert(as_str!(*e));});
     return element_set.len() < elements.len();
 }
 
